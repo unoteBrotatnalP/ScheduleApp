cff95b73879dd239f9cc60520cd62a4063c84a46
unoteBrotatnalP <michal.wendel.up@gmail.com>
2025-04-09 10:56:50 +0200

    Initial commit

---
+++ src/main/java/com/exitroom/scheduler_app/SchedulerAppApplication.java
@@ -0,0 +1,13 @@
+package com.exitroom.scheduler_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exitroom/scheduler_app/SchedulerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exitroom.scheduler_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
